d78ce9bcc312ced6eae7feceeae9a7434833f162
arnovikov <artem.novikov88@gmail.com>
2021-07-28 13:15:13 +0300

    Bug fixing

--- TELEGRAM_BOT.py
+++ TELEGRAM_BOT.py
@@ -106,7 +106,7 @@ def text_message(message):
 			bot.send_sticker(message.from_user.id, 'CAACAgIAAxkBAAECozdg_n1XvL7Pbcr--_5iIfg72IeoVwACAwADkp8eEXErN798EUMfIAQ')
 		else:
 			bot.send_sticker(message.from_user.id,'CAACAgIAAxkBAAECoyhg_nxSIPUY753MX_LTa21rfeTbJwAChwUAAiMFDQABE-Nhq6tbXOMgBA')
-			bot.send_message(message.from_user.id, 'Что-то я вас не понимаю, перепроверьте данные, которые вводите, пожалуйста. Для более детальной информации ты всегда можешь использовать команду /help')
+			bot.send_message(message.from_user.id, 'К сожалению, я вас не понимаю. Проверьте данные, которые вводите, пожалуйста. Для более детальной информации вы всегда можете использовать команду /help')
 
 
 @bot.message_handler(content_types=["document"])
