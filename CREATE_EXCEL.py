75fbd40c205946a14236442ba03ec75fe4dc39bd
arnovikov <artem.novikov88@gmail.com>
2021-12-13 14:37:08 +0300

    new field added

--- CREATE_EXCEL.py
+++ CREATE_EXCEL.py
@@ -7,10 +7,10 @@ def create_excel (mc_list,mc_data,file_name):
     file_path = str(config["CREATE_EXCEL"]["file_path"])+file_name
     wb = openpyxl.Workbook()
     ws = wb.active
-    ws.append(('cis','cis_status','gtin','owner_INN','owner_name','producer_INN','producer_name','emissionDate','emissionType','introducedDate','lastDocId','statusEx'))
+    ws.append(('cis','cis_status','gtin','owner_INN','owner_name','producer_INN','producer_name','emissionDate','emissionType','introducedDate','lastDocId','lastStatusChangeDate','statusEx'))
     for cell in mc_list:
         if cell in mc_data.keys():
-            ws.append((cell, mc_data.get(cell).get('status'), mc_data.get(cell).get('gtin'), mc_data.get(cell).get('ownerInn'), mc_data.get(cell).get('ownerName'), mc_data.get(cell).get('producerInn'), mc_data.get(cell).get('producerName'), mc_data.get(cell).get('emissionDate'), mc_data.get(cell).get('emissionType'), mc_data.get(cell).get('introducedDate'), mc_data.get(cell).get('lastDocId'), mc_data.get(cell).get('statusEx')))
+            ws.append((cell, mc_data.get(cell).get('status'), mc_data.get(cell).get('gtin'), mc_data.get(cell).get('ownerInn'), mc_data.get(cell).get('ownerName'), mc_data.get(cell).get('producerInn'), mc_data.get(cell).get('producerName'), mc_data.get(cell).get('emissionDate'), mc_data.get(cell).get('emissionType'), mc_data.get(cell).get('introducedDate'), mc_data.get(cell).get('lastDocId'), mc_data.get(cell).get('lastStatusChangeDate'), mc_data.get(cell).get('statusEx')))
         else:
             ws.append((cell,'-','-','-','-','-','-','-','-','-','-','-'))
     ws.auto_filter.ref = ws.dimensions
