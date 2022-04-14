c0166fb560890d4ef4bab9a34deeb6f15c07ab25
arnovikov <artem.novikov88@gmail.com>
2022-04-14 11:31:19 +0300

    Change the way of getting CRPT token

--- API_CRPT.py
+++ API_CRPT.py
@@ -3,22 +3,23 @@ def check_mc (mc_list):
     import requests
     import json
     import configparser
-    import subprocess
-    import datetime
-    import os
+    #import subprocess
+    #import datetime
+    #import os
+    from API_TT import get_CRPT_token
     config = configparser.ConfigParser()
     config.read(global_var())
     url = config["API_CRPT"]["url_check_mc"]
-    token_path = config["API_CRPT"]["token_path"]
-    script_path = config["API_CRPT"]["script_path"]
     mc_maxamount_to_check = int(config["API_CRPT"]["mc_maxamount_to_check"])
-    file_time = datetime.datetime.fromtimestamp(os.stat(token_path).st_mtime)
-    delta_time = datetime.timedelta(hours=10)
-    if file_time+delta_time <= datetime.datetime.now():
-        subprocess.check_call('powershell -file "' + script_path + '"')
-    token_file = open(token_path,'r',encoding='utf_16')
-    bearer = token_file.read()
-    token_file.close()
+    #token_path = config["API_CRPT"]["token_path"]
+    #script_path = config["API_CRPT"]["script_path"]
+    #file_time = datetime.datetime.fromtimestamp(os.stat(token_path).st_mtime)
+    #delta_time = datetime.timedelta(hours=10)
+    #if file_time+delta_time <= datetime.datetime.now():
+    #    subprocess.check_call('powershell -file "' + script_path + '"')
+    #token_file = open(token_path,'r',encoding='utf_16')
+    bearer = get_CRPT_token()
+    #token_file.close()
     data = {}
     count = 1
     num_of_runs = len(mc_list)//mc_maxamount_to_check + 1
@@ -35,21 +36,22 @@ def check_doc_status(doc_id):
     import requests
     import json
     import configparser
-    import subprocess
-    import datetime
-    import os
+    # import subprocess
+    # import datetime
+    # import os
+    from API_TT import get_CRPT_token
     config = configparser.ConfigParser()
     config.read(global_var())
     url2 = config["API_CRPT"]["url_check_doc"]
-    token_path = config["API_CRPT"]["token_path"]
-    script_path = config["API_CRPT"]["script_path"]
-    file_time = datetime.datetime.fromtimestamp(os.stat(token_path).st_mtime)
-    delta_time = datetime.timedelta(hours=10)
-    if file_time + delta_time <= datetime.datetime.now():
-        subprocess.check_call('powershell -file "' + script_path + '"')
-    token_file = open(token_path, 'r', encoding='utf_16')
-    bearer = token_file.read()
-    token_file.close()
+    # token_path = config["API_CRPT"]["token_path"]
+    # script_path = config["API_CRPT"]["script_path"]
+    # file_time = datetime.datetime.fromtimestamp(os.stat(token_path).st_mtime)
+    # delta_time = datetime.timedelta(hours=10)
+    # if file_time + delta_time <= datetime.datetime.now():
+    #     subprocess.check_call('powershell -file "' + script_path + '"')
+    # token_file = open(token_path, 'r', encoding='utf_16')
+    bearer = get_CRPT_token()
+    #token_file.close()
     headers = {'Authorization': 'Bearer ' + bearer, 'content-type': 'application/json', 'charset': 'utf-8'}
     req = requests.get(url2 + doc_id + '/body', headers=headers)
     doc_status = json.dumps(req.json()['downloadStatus'])
--- API_TT.py
+++ API_TT.py
@@ -18,3 +18,18 @@ def check_outs_doc_status(doc_number):
     outDocType = request.json()['content'][0]['outDocType']
     data_for_user = {'guid': guid, 'checkStatus': checkStatus, 'action': action, 'outDocType': outDocType}
     return data_for_user
+
+def get_CRPT_token():
+    import requests
+    import configparser
+    config = configparser.ConfigParser()
+    config.read(global_var())
+    url = config["API_TT"]["url_get_CRPT_token"]
+    token_path = config["API_TT"]["token_path"]
+    token_file = open(token_path, 'r', encoding='utf_16')
+    bearer = token_file.read()
+    token_file.close()
+    headers = {'X-Auth-Token': 'Bearer ' + bearer, 'Content-Type': 'application/json'}
+    request = requests.get(url, headers=headers)
+    token = request.json()['token']
+    return token
